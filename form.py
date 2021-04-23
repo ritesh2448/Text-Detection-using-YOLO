9aeffec17ba189dd9d73498cd2f32c1f0e16c872
Kritarth Jain <55143359+krucx@users.noreply.github.com>
2021-04-23 16:55:08 +0530

    Delete form.py

--- form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField,FileRequired
-from wtforms import SubmitField
-
-class ImageUploadForm(FlaskForm):
-
-    image = FileField('Upload Image',validators=[FileRequired()])
-    submit = SubmitField('Get text!')
-
